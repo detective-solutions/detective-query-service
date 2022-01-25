3bbbf982113bbae624de969b62dba757e3954d21
Maxim Perl <maxim.perl@outlook.de>
2022-01-25 17:43:44 +0100

    fix(DET-778): add pyodbc to requirements.txt

---
+++ src/detective_query_service/testfile.py
@@ -0,0 +1,11 @@
+from detective_query_service.connectors.sql.sql_connector import SQLConnector
+
+connection = SQLConnector(
+    host="detective-azure-sql-server.database.windows.net",
+    user="detective-server",
+    password="iqPUjn9RPmcU9Qk",
+    database="ms-sql-server-test",
+    db_type="mssql"
+)
+
+print(connection)
