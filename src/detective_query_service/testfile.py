dc8fe3fed8c3a3c4b1a1c11ca88228c042a338b3
Maxim Perl <maxim.perl@outlook.de>
2022-02-11 12:33:53 +0100

    refactor(DET-778): tests.yml install ms server ODBC dirver 13

--- src/detective_query_service/testfile.py
+++ src/detective_query_service/testfile.py
@@ -1,4 +1,14 @@
 
 import pyodbc
 print([x for x in pyodbc.drivers()])
-
+"""
+mssql:
+image: mcr.microsoft.com/mssql/server:2019-latest
+ports:
+- 1433:1433
+env:
+MSSQL_DATABASE: ${{ env.SQL_DATABASE }}
+MSSQL_USER: ${{ env.SQL_USER }}
+MSSQL_PASSWORD: ${{ env.SQL_PASSWORD }}
+MSSQL_ROOT_PASSWORD: ${{ env.SQL_PASSWORD }}
+"""
