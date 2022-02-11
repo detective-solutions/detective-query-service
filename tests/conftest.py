90e95d22725a0187aeeb418d2da7a77529668481
Maxim Perl <maxim.perl@outlook.de>
2022-02-11 20:09:48 +0100

    refactor(DET-778): try to install sql server driver 21

--- tests/conftest.py
+++ tests/conftest.py
@@ -48,7 +48,7 @@ def database_configs():
         },
         "mssql": {
             "host": "0.0.0.0",
-            "user": "test_user",
+            "user": "root",
             "password": "hsHlanZ0283819!lsH",
             "database": "testdb",
             "port": 1433,
