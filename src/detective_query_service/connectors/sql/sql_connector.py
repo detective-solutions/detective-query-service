f9f682ad3ad26fa37d44d8e54f28ab6129b33fe6
Maxim Perl <maxim.perl@outlook.de>
2022-02-14 17:08:49 +0100

    refactor(DET-778): add more precice msg for __repr__ debugging prupose

--- src/detective_query_service/connectors/sql/sql_connector.py
+++ src/detective_query_service/connectors/sql/sql_connector.py
@@ -41,6 +41,7 @@ class SQLConnector(Connector):
             return "True"
         else:
             # return f"{self._get_connection_string()} - established: {self.connection_established}"
+            msg = self.__error_status.split("FATAL")[1]
             return self.__error_status
 
 
