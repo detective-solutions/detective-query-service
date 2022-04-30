1a2767be68de434a0a408c1d1d356cbd1377e377
phips-git <p.mueller93@gmail.com>
2022-04-30 15:00:51 +0200

    feat(DET-851): open all network interfaces for container use

--- src/detective_query_service/settings.py
+++ src/detective_query_service/settings.py
@@ -2,8 +2,8 @@
 import pydgraph
 
 # Variables
-DGRAPH_HOST = 'localhost:9080'
-KAFKA_HOST = 'localhost:9093'
+DGRAPH_HOST = '0.0.0.0:9080'
+KAFKA_HOST = '0.0.0.0:9093'
 
 # set dgraph connection
 dgraph_client_stub = pydgraph.DgraphClientStub(DGRAPH_HOST)
